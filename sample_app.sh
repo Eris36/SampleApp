4ef15dcd8c27c7f84e72073c0fd7a029423d3e65
Eris36 <olegbait@yandex.ru>
2023-05-25 15:39:37 +0400

    jenkils add in container

--- sample_app.sh
+++ sample_app.sh
@@ -22,5 +22,5 @@ echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker run --rm  -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
